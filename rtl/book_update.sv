3e581d19f0a558e2b0166c16fcd4f2856887e7e4
Cai Jitong <jc5824@ic.ac.uk>
2026-06-24 00:24:23 +0100

    fix existing bugs

--- rtl/book_update.sv
+++ rtl/book_update.sv
@@ -14,10 +14,10 @@ module book_update
     output logic [WINDOW_SIZE - 1 : 0] bid_mask,
     output logic [WINDOW_SIZE - 1 : 0] ask_mask,
     //access to qty of a given address
-    input  logic [ADDR_WIDTH -1 : 0] read_addr_bid,
-    output logic [31 : 0]                    read_bid_qty,
-    input  logic [ADDR_WIDTH -1 : 0] read_addr_ask,
-    output logic [31 : 0]                    read_ask_qty
+    input  logic [ADDR_WIDTH -1 : 0] read_bid_addr,
+    output logic [31 : 0]            read_bid_qty,
+    input  logic [ADDR_WIDTH -1 : 0] read_ask_addr,
+    output logic [31 : 0]            read_ask_qty
 );
 
     logic [31:0] bid_array [WINDOW_SIZE];
@@ -32,8 +32,9 @@ module book_update
     state_e curr_state, next_state;
 
     logic [ADDR_WIDTH -1 : 0] addr;
-    fm24_cmd_t                          reg_cmd;
-    logic [31:0]                        old_qty;
+    fm24_cmd_t                reg_cmd;
+    logic [31:0]              old_qty;
+    logic [31:0]              new_qty;
 
     always_comb begin
         //comb logic: fsm state machine, compute addr from cmd.price, compare tob
@@ -83,7 +84,6 @@ module book_update
                     end          
                 end
                 WRITE: begin
-                    logic [31:0] new_qty;
                      if (is_remove_op(reg_cmd.msg_type))
                         new_qty = (old_qty > reg_cmd.qty) ? old_qty - reg_cmd.qty : '0;
                     else
@@ -106,8 +106,8 @@ module book_update
             read_bid_qty <= '0;
             read_ask_qty <= '0;
         end else begin
-            read_bid_qty <= bid_array[read_addr_bid];
-            read_ask_qty <= ask_array[read_addr_ask];
+            read_bid_qty <= bid_array[read_bid_addr];
+            read_ask_qty <= ask_array[read_ask_addr];
         end
     end
 
